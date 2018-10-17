0e5b8cbc06802bcaa78d7220ce27d55f5916c348
supersem <xsupersemx@gmail.com>
2018-10-17 16:47:28 +0300

    Middle commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Project.create(name: Faker::Name.first_name)
+end
+
+Project.all.each do |project|
+  Faker::Number.between(5, 10).times do
+    project.tasks.create(title: Faker::Name.name, description: Faker::Lorem.paragraph)
+  end
+end
