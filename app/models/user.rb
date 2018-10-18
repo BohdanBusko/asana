916daffc4b4e28e10d45c0bc6a5f139e17ee9f95
ShevchukTania <taniabeley@ukr.net>
2018-10-18 12:17:03 +0300

    changed facebook def

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,19 +6,32 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[facebook]
 
   def self.from_omniauth(auth)
-    user = User.where(email: auth.info.email).first
-    if user
-      return user
-    else
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.password = Devise.friendly_token[0,20]
-        user.full_name = auth.info.name
-        user.uid = auth.uid
-        user.provider = auth.provider
-        user.oauth_token = auth.credentials.token
-        user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-        user.save!
-        # user.skip_confirmation!
+    if request.env["omniauth.auth"].info.email.blank?
+      redirect_to "/users/auth/facebook?auth_type=rerequest&scope=email"
+      return
+        user = User.where(email: auth.info.email).first
+        if user
+          return user
+        else
+          where(provider: auth.provider, uid: auth.uid).first do |user|
+            user.password = Devise.friendly_token[0,20]
+            user.full_name = auth.info.name
+            user.email = auth.info.email ||= noemail@email.com
+            user.uid = auth.uid
+            user.provider = auth.provider
+            user.oauth_token = auth.credentials.token
+            user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+            user.save!
+            # user.skip_confirmation!
+          end
+        end
+      end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
       end
     end
   end
