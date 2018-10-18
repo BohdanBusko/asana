4bddecde78e073d8e5612f64f2efefb79e39533f
Busko Bogdan <busko.bogdan@gmail.com>
2018-10-18 08:58:43 +0300

    add projects

---
+++ app/controllers/account/projects_controller.rb
@@ -0,0 +1,50 @@
+class Account::ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @projects = current_user.projects.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @project = current_user.projects.new
+  end
+
+  def create
+    @project = current_user.projects.new(project_params)
+
+    if @project.save
+      redirect_to account_projects_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update_attributes(project_params)
+      redirect_to account_projects_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project.delete
+    redirect_to account_projects_path
+  end
+
+  private
+
+  def set_project
+    @project = current_user.projects.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
               path: '',
               path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
               controllers: {omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
+
+  namespace :account do
+    resources :projects
+  end
 end
